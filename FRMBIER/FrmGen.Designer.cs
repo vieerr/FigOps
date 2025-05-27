3a0eef4579eaef1a5f7303e782004bbaa7764aff
Olivier Paspuel <olivier.paspuel@gmail.com>
2025-05-26 20:17:00 -0500

    feat: single file functionality test

---
+++ FRMBIER/FrmGen.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FRMBIER
+{
+    partial class FrmGen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rotationTrackBar = new System.Windows.Forms.TrackBar();
+            this.scaleTrackBar = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlParametros = new System.Windows.Forms.Panel();
+            this.cmbFig = new System.Windows.Forms.ComboBox();
+            this.picCnv = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rotationTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCnv)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.rotationTrackBar);
+            this.panel1.Controls.Add(this.scaleTrackBar);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pnlParametros);
+            this.panel1.Controls.Add(this.cmbFig);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // rotationTrackBar
+            // 
+            this.rotationTrackBar.Location = new System.Drawing.Point(0, 361);
+            this.rotationTrackBar.Maximum = 360;
+            this.rotationTrackBar.Name = "rotationTrackBar";
+            this.rotationTrackBar.Size = new System.Drawing.Size(249, 45);
+            this.rotationTrackBar.TabIndex = 6;
+            this.rotationTrackBar.Value = 5;
+            // 
+            // scaleTrackBar
+            // 
+            this.scaleTrackBar.Location = new System.Drawing.Point(4, 297);
+            this.scaleTrackBar.Maximum = 30;
+            this.scaleTrackBar.Minimum = 5;
+            this.scaleTrackBar.Name = "scaleTrackBar";
+            this.scaleTrackBar.Size = new System.Drawing.Size(245, 45);
+            this.scaleTrackBar.TabIndex = 5;
+            this.scaleTrackBar.Value = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // pnlParametros
+            // 
+            this.pnlParametros.Location = new System.Drawing.Point(4, 31);
+            this.pnlParametros.Name = "pnlParametros";
+            this.pnlParametros.Size = new System.Drawing.Size(249, 181);
+            this.pnlParametros.TabIndex = 3;
+            // 
+            // cmbFig
+            // 
+            this.cmbFig.FormattingEnabled = true;
+            this.cmbFig.Items.AddRange(new object[] {
+            "Rombo",
+            "Pentágono",
+            "Romboide",
+            "Trapezoide"});
+            this.cmbFig.Location = new System.Drawing.Point(3, 3);
+            this.cmbFig.Name = "cmbFig";
+            this.cmbFig.Size = new System.Drawing.Size(250, 21);
+            this.cmbFig.TabIndex = 2;
+            this.cmbFig.SelectedIndexChanged += new System.EventHandler(this.cmbFig_SelectedIndexChanged);
+            // 
+            // picCnv
+            // 
+            this.picCnv.Location = new System.Drawing.Point(3, 3);
+            this.picCnv.Name = "picCnv";
+            this.picCnv.Size = new System.Drawing.Size(508, 420);
+            this.picCnv.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCnv.TabIndex = 0;
+            this.picCnv.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.picCnv);
+            this.panel2.Location = new System.Drawing.Point(274, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(514, 426);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Escala";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Rotación";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Para mover la figura, mover con las flechas del teclado";
+            // 
+            // FrmGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmGen";
+            this.Text = "Figuras";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rotationTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCnv)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlParametros;
+        private System.Windows.Forms.ComboBox cmbFig;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picCnv;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar scaleTrackBar;
+        private System.Windows.Forms.TrackBar rotationTrackBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FRMBIER/FrmGen.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FRMBIER
+{
+    public partial class FrmGen : Form
+    {
+
+        private Button btnCalcular;
+        private Label lblArea, lblPerimetro, lblScale, lblRotation;
+        private float scale = 0.001f;
+        private float rotation = 0f;
+        private PointF position = new PointF(0, 0);
+        private string currentFigure = "";
+        private PointF[] currentPoints;
+
+        public FrmGen()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            btnCalcular = new Button { Text = "Calcular", Location = new Point(10, 300), Size = new Size(100, 30) };
+            btnCalcular.Click += btnCalcular_Click;
+            this.Controls.Add(btnCalcular);
+
+            lblArea = new Label { Location = new Point(120, 305), AutoSize = true };
+            lblPerimetro = new Label { Location = new Point(120, 330), AutoSize = true };
+
+            lblScale = new Label { Text = "Escala: 1.0x", Location = new Point(10, 370), AutoSize = true };
+
+            scaleTrackBar.Scroll += ScaleTrackBar_Scroll;
+            lblRotation = new Label { Text = "Rotación: 0°", Location = new Point(10, 410), AutoSize = true };
+       rotationTrackBar.Scroll += RotationTrackBar_Scroll;
+
+            this.Controls.AddRange(new Control[] {
+                lblArea, lblPerimetro,
+            });
+
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void ScaleTrackBar_Scroll(object sender, EventArgs e)
+        {
+            scale = scaleTrackBar.Value / 10f;
+            lblScale.Text = $"Escala: {scale:F1}x";
+            RedrawFigure();
+        }
+
+        private void RotationTrackBar_Scroll(object sender, EventArgs e)
+        {
+            rotation = rotationTrackBar.Value;
+            lblRotation.Text = $"Rotación: {rotation}°";
+            RedrawFigure();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            const float moveStep = 10f;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up: position.Y -= moveStep; break;
+                case Keys.Down: position.Y += moveStep; break;
+                case Keys.Left: position.X -= moveStep; break;
+                case Keys.Right: position.X += moveStep; break;
+                case Keys.Add:
+                case Keys.Oemplus:
+                    scaleTrackBar.Value = Math.Min(scaleTrackBar.Maximum, scaleTrackBar.Value + 1);
+                    break;
+                case Keys.Subtract:
+                case Keys.OemMinus:
+                    scaleTrackBar.Value = Math.Max(scaleTrackBar.Minimum, scaleTrackBar.Value - 1);
+                    break;
+                case Keys.Q:
+                    rotationTrackBar.Value = (rotationTrackBar.Value - 5 + 360) % 360;
+                    break;
+                case Keys.E:
+                    rotationTrackBar.Value = (rotationTrackBar.Value + 5) % 360;
+                    break;
+                default: return;
+            }
+
+            RedrawFigure();
+            e.Handled = true;
+        }
+
+        private void RedrawFigure()
+        {
+            if (currentPoints == null) return;
+
+            Bitmap bmp = new Bitmap(picCnv.Width, picCnv.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.TranslateTransform(picCnv.Width / 2 + position.X, picCnv.Height / 2 + position.Y);
+                g.ScaleTransform(scale, scale);
+                g.RotateTransform(rotation);
+
+                using (Pen pen = new Pen(Color.Blue, 1))
+                {
+                    g.DrawPolygon(pen, currentPoints);
+                }
+            }
+            picCnv.Image = bmp;
+        }
+
+        private void cmbFig_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pnlParametros.Controls.Clear();
+            currentFigure = cmbFig.SelectedItem?.ToString();
+            if (currentFigure == null) return;
+
+            switch (currentFigure)
+            {
+                case "Rombo":
+                    AddParametro("Diagonal 1", "txtD1");
+                    AddParametro("Diagonal 2", "txtD2");
+                    break;
+                case "Pentágono":
+                    AddParametro("Lado", "txtLado");
+                    break;
+                case "Romboide":
+                    AddParametro("Base", "txtBase");
+                    AddParametro("Altura", "txtAltura");
+                    break;
+                case "Trapezoide":
+                    AddParametro("Base mayor", "txtBMayor");
+                    AddParametro("Base menor", "txtBMenor");
+                    AddParametro("Altura", "txtAltura");
+                    break;
+            }
+            RedrawFigure();
+        }
+
+        private void AddParametro(string labelText, string textboxName)
+        {
+            int y = pnlParametros.Controls.Count * 30;
+            pnlParametros.Controls.Add(new Label { Text = labelText, Location = new Point(10, y) });
+            pnlParametros.Controls.Add(new TextBox { Name = textboxName, Location = new Point(120, y), Width = 130 });
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFigure))
+            {
+                MessageBox.Show("Selecciona una figura primero.");
+                return;
+            }
+
+            try
+            {
+                float area = 0, perimetro = 0;
+                float.TryParse(GetParamValue("scale").ToString(), out scale);
+
+                switch (currentFigure)
+                {
+                    case "Rombo":
+                        float d1 = GetParamValue("txtD1");
+                        float d2 = GetParamValue("txtD2");
+                        area = (d1 * d2) / 2;
+                        perimetro = 4 * (float)Math.Sqrt((d1 * d1 + d2 * d2) / 2);
+                        currentPoints = CreateRhombusPoints(d1, d2);
+                        break;
+                    case "Pentágono":
+                        float lado = GetParamValue("txtLado");
+                        area = (5 * lado * lado) / (4 * (float)Math.Tan(Math.PI / 5));
+                        perimetro = 5 * lado;
+                        currentPoints = CreatePentagonPoints(lado);
+                        break;
+                    case "Romboide":
+                        float b = GetParamValue("txtBase");
+                        float h = GetParamValue("txtAltura");
+                        area = b * h;
+                        perimetro = 2 * (b + h);
+                        currentPoints = CreateParallelogramPoints(b, h);
+                        break;
+                    case "Trapezoide":
+                        float B = GetParamValue("txtBMayor");
+                        float bm = GetParamValue("txtBMenor");
+                        float alt = GetParamValue("txtAltura");
+                        area = (B + bm) * alt / 2;
+                        perimetro = B + bm + 2 * alt;
+                        currentPoints = CreateTrapezoidPoints(B, bm, alt);
+                        break;
+                }
+
+                lblArea.Text = $"Área: {area:F2}";
+                lblPerimetro.Text = $"Perímetro: {perimetro:F2}";
+                RedrawFigure();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private float GetParamValue(string name)
+        {
+            var control = pnlParametros.Controls.Find(name, false);
+            if (control.Length == 0 || !float.TryParse(control[0].Text, out float value))
+                return 50f; // Default value
+            return value;
+        }
+
+        private PointF[] CreateRhombusPoints(float d1, float d2)
+        {
+            return new PointF[] {
+                new PointF(0, -d2/2),
+                new PointF(d1/2, 0),
+                new PointF(0, d2/2),
+                new PointF(-d1/2, 0)
+            };
+        }
+
+        private PointF[] CreatePentagonPoints(float side)
+        {
+            PointF[] points = new PointF[5];
+            float R = side / (2 * (float)Math.Sin(Math.PI / 5));
+            for (int i = 0; i < 5; i++)
+            {
+                double angle = -Math.PI / 2 + i * 2 * Math.PI / 5;
+                points[i] = new PointF(
+                    R * (float)Math.Cos(angle),
+                    R * (float)Math.Sin(angle)
+                );
+            }
+            return points;
+        }
+
+        private PointF[] CreateParallelogramPoints(float b, float h)
+        {
+            return new PointF[] {
+                new PointF(-b/2, -h/2),
+                new PointF(b/2, -h/2),
+                new PointF(b/2 - b/4, h/2),
+                new PointF(-b/2 - b/4, h/2)
+            };
+        }
+
+        private PointF[] CreateTrapezoidPoints(float B, float b, float h)
+        {
+            return new PointF[] {
+                new PointF(-B/2, h/2),
+                new PointF(B/2, h/2),
+                new PointF(b/2, -h/2),
+                new PointF(-b/2, -h/2)
+            };
+        }
+
+    }
+}
